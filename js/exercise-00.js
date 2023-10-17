6cf3a559cfd14965f13cc55fe5e93676efc5f952
Nil Farras <farrasnil@gmail.com>
2023-10-17 20:25:26 +0200

    ex 1 completed

---
+++ js/exercise-00.js
@@ -0,0 +1,23 @@
+/* 
+   Create a constant variable of each of the following primitive types:
+   - String
+   - Number
+   - Boolean
+   - Null
+   - Undefined
+
+   Log out each variable to the terminal at the end.
+*/
+
+const Nil_string = "Hello";
+const Nil_number = 22;
+const Nil_boolean = true;
+const Nil_null = null;
+const Nil_undefined = undefined;
+
+console.log(Nil_string);
+console.log(Nil_number);
+console.log(Nil_boolean);
+console.log(Nil_null);
+console.log(Nil_undefined);
+
